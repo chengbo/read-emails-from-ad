57715051091292b33cc322bcc08f44d5165ae111
Roy Cheng <roy.b.cheng@newegg.com>
2014-04-04 22:21:08 +0100

    implement get all mailgroups from ad

--- ActiveDirectory.cs
+++ ActiveDirectory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
 
 namespace ReadEmailsFromAD
 {
@@ -7,7 +8,91 @@ namespace ReadEmailsFromAD
     {
         public List<MailGroup> GetAllMailGroups()
         {
-            throw new NotImplementedException();
+            var resultTable = new List<MailGroup>();
+
+            string filter =
+                String.Format(
+                    "(&(objectClass=Group)(objectCategory=Group)(|(msExchHideFromAddressLists=FALSE)(!(msExchHideFromAddressLists=*))))");
+
+            SearchResultCollection searchResults = FindAll(filter, new[] { "displayName", "mail", "mailNickname", "name" });
+            if (searchResults == null)
+            {
+                return resultTable;
+            }
+
+            for (int i = 0; i < searchResults.Count; i++)
+            {
+                var searchResult = searchResults[i];
+
+                string propDisplayName = GetValue(searchResult, "displayName");
+                string propEmailAddress = GetValue(searchResult, "mail");
+                string propAliasName = GetValue(searchResult, "mailNickname");
+                string propName = GetValue(searchResult, "name");
+
+                if (!String.IsNullOrEmpty(propDisplayName))
+                {
+                    resultTable.Add(new MailGroup
+                    {
+                        DisplayName = propDisplayName,
+                        EmailAddress = propEmailAddress,
+                        AliasName = propAliasName,
+                        Name = propName
+                    });
+                }
+            }
+
+            return resultTable;
+        }
+
+        private SearchResultCollection FindAll(string filter, string[] propertiesToLoad)
+        {
+            DirectorySearcher searcher = null;
+            try
+            {
+                searcher = CreateSearcher(filter, propertiesToLoad);
+                return searcher.FindAll();
+            }
+            finally
+            {
+                if (searcher != null)
+                {
+                    if (searcher.SearchRoot != null)
+                    {
+                        searcher.SearchRoot.Dispose();
+                    }
+                    searcher.Dispose();
+                }
+            }
+        }
+
+        private DirectorySearcher CreateSearcher(string filter, string[] propertiesToLoad)
+        {
+            var searcher = new DirectorySearcher
+            {
+                SearchRoot = new DirectoryEntry(),
+                SearchScope = SearchScope.Subtree,
+                Filter = filter
+            };
+            if (propertiesToLoad != null)
+            {
+                searcher.PropertiesToLoad.AddRange(propertiesToLoad);
+            }
+
+            return searcher;
+        }
+
+        private static string GetValue(SearchResult searchResult, string propertyName)
+        {
+            if (searchResult.Properties[propertyName] == null || searchResult.Properties[propertyName].Count == 0)
+            {
+                return String.Empty;
+            }
+
+            object prop = searchResult.Properties[propertyName][0];
+
+            var value = prop as string;
+
+            return value ?? prop.ToString();
         }
     }
 }
